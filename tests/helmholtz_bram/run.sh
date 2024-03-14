4dbb6fac6fd938acb7104700f085e9127b7d5b77
Stephanie Soldavini <stephanie.soldavini@polimi.it>
2024-03-14 22:59:00 +0000

    fixes

--- tests/helmholtz_bram/run.sh
+++ tests/helmholtz_bram/run.sh
@@ -3,5 +3,7 @@ VITIS=/opt/xilinx/Vitis/2021.1
 OPATH=../../src
 OUT=out
 ${OPATH}/olympus.py -p ${OPATH}/targetplatform/alveo/alveo_u280.json -a ./helmholtz.mlir -o $OUT
-source $VITIS/settings64.sh
-vitis_hls ${OPATH}/scripts/omm_hls.tcl $OUT/src/helm_top_CU.cpp
+if [ $? -eq 0 ]; then
+    source $VITIS/settings64.sh
+    vitis_hls ${OPATH}/scripts/omm_hls.tcl $OUT/src/helm_top_CU.cpp
+fi
